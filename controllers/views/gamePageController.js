9102e837ff717be9d788e2599fd4f953cf0c85b3
Nathan Woodring <joshjone44@outlook.com>
2025-02-10 14:13:29 -0500

    commit

--- controllers/views/gamePageController.js
+++ controllers/views/gamePageController.js
@@ -2,16 +2,44 @@ const axios = require("axios");
 const settings = require("../../config/settings");
 async function renderGamePage(req, res) {
     const response = await axios.get(
-        `${settings.ROOT}:${settings.PORT}/api/games/${req.params.id}`,
-        {
-            params: req.query,
-        }
+        `${settings.ROOT}:${settings.PORT}/api/games/${req.params.id}`
     );
+ 
     data = response.data;
-
+    const cover = await endPointGameID("covers", req.params.id);
+    data.cover = cover[0] ? cover[0].url : "";
+    data.genres = await endPointGameID("genres", req.params.id);
+    data.gameModes = await endPointGameID("gamemodes", req.params.id);
+    data.Platforms = await endPointGameID("platforms", req.params.id);
+    data.Characters = await endPointGameID("characters", req.params.id);
+    data.Websites = await endPointGameID("websites", req.params.id);
+    data.Screenshots = await endPointGameID("screenshots", req.params.id);
+    data.games = await endPointGameID("similar", req.params.id);
+ 
+    for(let game of data.games) {
+        const cover = await axios.get(
+            `${settings.ROOT}:${settings.PORT}/api/covers`,
+        { params: {gameid: game.similar_game_id} }
+    );
+    game.cover = cover.data[0] ? cover.data[0].url : "";
+    }
+ 
+ 
     res.render("game", {
         title: "Games!",
-        gameData: data, 
+        gameData: data,
     });
 }
+ 
+async function endPointGameID(endpoint, gameId){
+    const values = await axios.get(
+        `${settings.ROOT}:${settings.PORT}/api/${endpoint}`,
+        {
+            params: { gameid: gameId },
+        }
+    );
+    return values.data;
+ 
+}
+ 
 module.exports = { renderGamePage };
--- models/platform.js
+++ models/platform.js
@@ -1,7 +1,14 @@
 const db = require("../config/db");
+async function getAllPlatforms(start = 0, limit = 50, gameid) {
+    
+let condition = ``;
+    let join = "";
+    if (gameid) {
+        condition = db.format(` where gp.game_id = ? `, [gameid]);
+        join = `inner join game_platform gp on gp.platform_id = p.platform_id`;
+    }
 
-async function getAllPlatforms(start = 0, limit = 50) {
-    const [rows] = await db.execute("Select * from platforms Limit ?,?", [
+    const [rows] = await db.execute(`Select * from platforms p ${join} ${condition} Limit ?,?`, [
         start.toString(), //tostring for workaround of mysql 8.4 bug
         limit.toString(),
     ]);
@@ -15,5 +22,6 @@ async function getPlatformById(platformId) {
     );
     return rows[0];
 }
+
 
 module.exports = { getAllPlatforms, getPlatformById };
